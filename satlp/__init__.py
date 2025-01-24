01ecbd964604f481568ef8acb1bd1c91b5777476
João Lobo <j.lobo.pev@gmail.com>
2025-01-24 12:25:42 -0300

    Correct class names in __init__.py

--- satlp/__init__.py
+++ satlp/__init__.py
@@ -1,8 +1,8 @@
 from satlp.cnf_loader import CNFLoader
 from satlp.sat_baseclass import SATasLP
 from satlp.lp_formulations import (
-    SATasLPOriginal,
-    SATasLPWithFixing,
-    SATasMILP,
-    SATasMILPSimple
+    SATasLPOptimization,
+    SATasLPFeasibility,
+    SATasMILPOptimization,
+    SATasMILPFeasibility
 )
