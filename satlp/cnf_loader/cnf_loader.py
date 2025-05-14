8a81a560609c1a7dacaca0271d3870c0afeb4d8e
João Lobo <j.lobo.pev@gmail.com>
2025-05-14 09:49:47 -0300

    Start to implement clause simplificaiton tools

--- satlp/cnf_loader/cnf_loader.py
+++ satlp/cnf_loader/cnf_loader.py
@@ -1,4 +1,4 @@
-from numpy import array as np_array
+import numpy as np
 class CNFLoader():
 
     def __init__(self, filename=None):
@@ -7,7 +7,6 @@ class CNFLoader():
         self.clauses = None
         if filename:
             self._load_from_file(filename)
-
 
     def _load_from_file(self, filename):
 
@@ -18,7 +17,7 @@ class CNFLoader():
             m_clauses = int(cnf_data[0].split()[-1])
             n_variables = int(cnf_data[0].split()[-2])
             clauses = [
-                [int(i) for i in l.split()[:-1]]
+                sorted([int(i) for i in l.split()[:-1]], key=abs)
                     for l in cnf_data[1:] if len(l) > 0
             ]
 
@@ -34,3 +33,27 @@ class CNFLoader():
             except FileError:
                 print("Something went wrong loading the file. Make sure it is in DIMCAS formats.")
 
+    def simplify(self, assignments):
+        simple_clauses = []
+        for c in self.clauses:
+            x = np.in1d(assignments, c)
+            if not x.any():
+                not_x = np.in1d(-assignments, c)
+                if not_x.any():
+                    relevant = -assignments[not_x]
+                    simple_c = [c[i] for i in range(len(c)) if c[i] not in relevant]
+                    simple_clauses.append(simple_c)                    
+
+        return simple_clauses
+
+    def simplify_pair(self, p, s_clauses):
+        inf_assignments = []
+        for c in s_clauses:
+            if len(c) == 2:
+                if c != p and np.in1d(np.abs(c), np.abs(p)).sum() == 2:
+                    _c = np.array(c)
+                    _p = np.array(p)
+                    idx = (_c == _p)
+                    inf_assignments.append(_p[idx].item())
+
+        return inf_assignments        
